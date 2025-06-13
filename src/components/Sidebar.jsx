a80d63a5f35bb14a4c39ad369ad0c422ab26f324
aman-codde <amandeepk221@gmail.com>
2025-06-13 05:15:22 -0700

    feat: add Dashboard and Sidebar components with initial layout and styling

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import styles from './Sidebar.module.css';
+
+const Sidebar = ({setView,currentView}) => {
+    const menuItems = [
+        { label: 'Products', key: 'products'},
+        { label: 'Add Product', key: 'add'},
+        { label: 'Charts', key: 'charts'}
+    ]
+
+    return (
+        <div className={styles.sidebar}>
+            <div className={styles.title}>Admin Dashboard</div>
+            {menuItems.map((item) => (
+                <button 
+                key={item.key}
+                onClick={() => setView(item.key)}
+                className={`${styles.menuItem} ${currentView === item.key? styles.active:''}`}
+                >
+                    {item.label}
+                </button>
+            ))}
+        </div>
+    )
+}
+
+export default Sidebar;
