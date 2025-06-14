7d989f951f3dce9b14f7718f46108996ec583dff
jothiaishu <jothiaishu2311@gmail.com>
2025-06-14 09:29:53 -0400

    06/14:added product list

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,32 @@
+import React from "react";
+import products from "../data/products_data";
+
+
 const ProductList = () => {
     return (
-        <div>
-            Product list form
-        </div>
-    )
-}
+         <div style={{ padding: '20px' }}>
+      <h2>Investment Products</h2>
+      <table border="1" cellPadding="10" style={{ width: '100%', borderCollapse: 'collapse' }}>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>price ($)</th>
+            <th>stock</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+              <td>{product.stock}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default ProductList;
