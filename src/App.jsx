4751a4160d88e04ead88b95af93c0210ecc99d1d
sandeepkaurr <141114658+sandeepkaurr@users.noreply.github.com>
2025-06-16 13:03:12 -0700

    Sk add product form (#5)
    * updated
    
    * Resolved merge conflict in App.jsx
    
    * Finalize conflict resolution in AddProductForm.jsx
    
    * Resolved merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+
 import './App.css'
 
 import Dashboard from './pages/Dashboard'
 
 
+
 function App() {
-  
   return (
     <>
+
+  
       <Dashboard/>
      
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/AddProductForm.jsx
+++ src/components/AddProductForm.jsx
@@ -1,9 +1,82 @@
+import { useState } from "react";
+import "./AddProductForm.css";
+
 const AddProductForm = () => {
-    return (
-        <div>
-            Add product Form
-        </div>
-    )
-}
+  const [formData, setformData] = useState({
+    name: "",
+    category: "",
+    price: "",
+    stock: "",
+  });
+  const handleChange = (e) => {
+    setformData((prev) => ({
+      ...prev,
+      [e.target.name]: [e.target.value],
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    alert("Your Data Is Submitted Successfully!");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="formEle"
+          no
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={formData.name}
+          onChange={handleChange}
+        ></input>
+        <br></br>
+        <select
+          className="formEle"
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+          style={{ width: "90%" }}
+        >
+          <option value="">Select Category</option>
+          <option value="Electronics">Electronics</option>
+          <option value="Furniture">Furniture</option>
+          <option value="Clothing">Clothing</option>
+        </select>
+        <br></br>
+        <input
+          className="formEle"
+          type="number"
+          name="price"
+          placeholder="Price"
+          value={formData.price}
+          onChange={handleChange}
+          min="0"
+        ></input>
+        <br></br>
+        <input
+          className="formEle"
+          type="number"
+          name="stock"
+          placeholder="Stock"
+          value={formData.stock}
+          onChange={handleChange}
+          min="0"
+          //   git
+          //   push
+          //   origin
+          //   sk-add-product-form
+        ></input>
+        <br></br>
+        <button type="submit" className="btn">
+          Add Product
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default AddProductForm;
