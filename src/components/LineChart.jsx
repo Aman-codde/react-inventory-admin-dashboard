245e031408d62713b4b4caebc670fee1baf754a8
sandeep kaur <dhamiiii95@gmail.com>
2025-06-19 11:41:21 -0700

    Implement chart toggle with default BarChart view and styled buttons

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  LineChart as ReactLineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import products from "../data/products_data";
+
+const LineChart = () => (
+  <ResponsiveContainer width="100%" height={360}>
+    <ReactLineChart data={products}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="price" stroke="#8884d8" />
+      <Line type="monotone" dataKey="stock" stroke="#82ca9d" />
+    </ReactLineChart>
+  </ResponsiveContainer>
+);
+
+export default LineChart;
---
+++ src/components/PieChart.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  PieChart as ReactPieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import products from "../data/products_data";
+
+const COLORS = [
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "#AF19FF",
+  "#FF4560",
+  "#00E396",
+  "#775DD0",
+];
+const PieChart = () => {
+  return (
+    <ResponsiveContainer width="100%" height={360}>
+      <ReactPieChart>
+        <Pie
+          data={products}
+          cx="50%"
+          cy="50%"
+          outerRadius={100}
+          fill="#8884d8"
+          dataKey="stock"
+          nameKey={name}
+          label
+        >
+          {products.map((entry, index) => (
+            <Cell key={index} fill={COLORS[index % COLORS.length]}></Cell>
+          ))}
+        </Pie>
+        <Legend></Legend>
+        <Tooltip></Tooltip>
+      </ReactPieChart>
+    </ResponsiveContainer>
+  );
+};
+export default PieChart;
