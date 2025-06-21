404757299824ff94a7f5557ca6241105c1ba3c37
jothi <jothiaishu2311@gmail.com>
2025-06-21 09:38:35 -0400

    Added Search Bar to Filter Product List by Name #7 (#13)
    * 06/14:added product list
    
    * adding modified file
    
    * Added a search bar to the product list page.
    closed #7.

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,28 +1,50 @@
-import React from "react";
+import React, { useState } from "react"; 
 import products from "../data/products_data";
 
-
 const ProductList = () => {
-    return (
-         <div style={{ padding: '20px' }}>
-      <h2>Investment Products</h2>
+ 
+  const [searchTerm, setSearchTerm] = useState("");
+
+  
+  const filteredProducts = products.filter(product =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div style={{ padding: '20px' }}>
+      
+     
+      <input
+        type="text"
+        placeholder="Search items..."
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+        style={{ marginBottom: "1rem", padding: "0.5rem", width: "200px" }}
+      />
+
+      <h2>Inventory Products</h2>
       <table border="1" cellPadding="10" style={{ width: '100%', borderCollapse: 'collapse' }}>
         <thead>
           <tr>
-            <th>name</th>
-            <th>price ($)</th>
-            <th>stock</th>
+            <th>Name</th>
+            <th>Price ($)</th>
+            <th>Stock</th>
           </tr>
         </thead>
         <tbody>
-          {products.map((product) => (
-            <tr key={product.id}>
-              <td>{product.name}</td>
-              <td>{product.price}</td>
-              <td>{product.stock}
-              </td>
+          {filteredProducts.length > 0 ? (
+            filteredProducts.map((product) => (
+              <tr key={product.id}>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+                <td>{product.stock}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="3" style={{ textAlign: 'center' }}>No products found.</td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
     </div>
