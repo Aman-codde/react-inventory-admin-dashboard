e212caf9f65c23f2724a7c17adad77f3a1be7df8
aman-codde <amandeepk221@gmail.com>
2025-07-02 08:09:38 -0700

    Refactor ChartView to use shared styles and remove module file

--- src/components/ChartView.jsx
+++ src/components/ChartView.jsx
@@ -2,16 +2,17 @@ import React, { useState } from "react";
 import PieChart from "./PieChart";
 import BarChart from "./BarChart";
 import LineChart from "./LineChart";
-import styles from "./ChartView.module.css";
+import common from '../styles/common.module.css';
+
 function ChartView() {
   const [chart, setChart] = useState("bar");
+
   return (
     <div>
-      <h1 className={styles.heading}>Charts of Data</h1>
-      <div className={styles.buttonContainer}>
+      <h1 className={common.heading}>Charts of Data</h1>
+      <div className={common.buttonContainer}>
         <button onClick={() => setChart("bar")}>Bar Chart</button>
         <button onClick={() => setChart("line")}>Line Chart</button>
-
         <button onClick={() => setChart("pie")}>Pie Chart</button>
       </div>
 
